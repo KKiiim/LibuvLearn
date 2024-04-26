118aaa92d4a143baae51e370e16f9cfdab0b59bf
KKiiim <jerryfoooster@gmail.com>
2024-04-26 17:47:14 +0800

    fix linter

--- src/idle-basic/main.c
+++ src/idle-basic/main.c
@@ -4,30 +4,29 @@
 int64_t counter = 0;
 
 void wait_for_a_while(uv_idle_t* handle) {
-    counter++;
+  counter++;
 
-    if (counter >= 10e7)
-        uv_idle_stop(handle);
+  if (counter >= 10e7) uv_idle_stop(handle);
 }
 
 int main() {
-    uv_idle_t idler;
-    uv_idle_t idler1;
-    uv_idle_t idler2;
-    uv_idle_t idler3;
+  uv_idle_t idler;
+  uv_idle_t idler1;
+  uv_idle_t idler2;
+  uv_idle_t idler3;
 
-    uv_idle_init(uv_default_loop(), &idler);
-    uv_idle_init(uv_default_loop(), &idler1);
-    uv_idle_init(uv_default_loop(), &idler2);
-    uv_idle_init(uv_default_loop(), &idler3);
-    uv_idle_start(&idler, wait_for_a_while);
-    uv_idle_start(&idler1, wait_for_a_while);
-    uv_idle_start(&idler2, wait_for_a_while);
-    uv_idle_start(&idler3, wait_for_a_while);
+  uv_idle_init(uv_default_loop(), &idler);
+  uv_idle_init(uv_default_loop(), &idler1);
+  uv_idle_init(uv_default_loop(), &idler2);
+  uv_idle_init(uv_default_loop(), &idler3);
+  uv_idle_start(&idler, wait_for_a_while);
+  uv_idle_start(&idler1, wait_for_a_while);
+  uv_idle_start(&idler2, wait_for_a_while);
+  uv_idle_start(&idler3, wait_for_a_while);
 
-    printf("Idling...\n");
-    uv_run(uv_default_loop(), UV_RUN_DEFAULT);
+  printf("Idling...\n");
+  uv_run(uv_default_loop(), UV_RUN_DEFAULT);
 
-    uv_loop_close(uv_default_loop());
-    return 0;
+  uv_loop_close(uv_default_loop());
+  return 0;
 }
